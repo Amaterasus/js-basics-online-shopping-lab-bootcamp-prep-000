304b15159428d373297d2cf3006c99e0dde5f2f8
Skye Geraghty <skyetay03@googlemail.com>
2019-12-09 03:43:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,9 +49,11 @@ function removeFromCart(item)
 
 function placeOrder(cardNumber) 
 {
+  var validCard = false
   if(cardNumber != null)
   {
-    
+    validCard = true
+    cart
   }
   return cardNumber == null ? "Sorry, we don't have a credit card on file for you." :
   ""
