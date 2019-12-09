ab6899f615f37b6eaf8e9c871629c80c28f07005
Skye Geraghty <skyetay03@googlemail.com>
2019-12-09 03:11:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
 var cart = [];
 
-function getCart() {
+function getCart() 
+{
  return cart;
 }
 
-function setCart(c) {
+function setCart(c) 
+{
   cart = c;
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(item) 
+{
+  return cart.push({itemName : item, itemPrice : Math.floor(Math.random() * 101)}) ? `${item} has been added to your cart.` : `No item was added what happened?`
 }
 
-function viewCart() {
+function viewCart() 
+{
+  var object 
+  return cart.length === 0 ? `Your shopping cart is empty.` : object
+}
+
+function total() 
+{
   // write your code here
 }
 
-function total() {
+function removeFromCart(item) 
+{
   // write your code here
 }
 
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
+function placeOrder(cardNumber) 
+{
   // write your code here
 }
