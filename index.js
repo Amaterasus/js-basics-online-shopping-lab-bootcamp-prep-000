d0c0d95823d1e17fb6ecf3e64543e2f69b5e5b83
Skye Geraghty <skyetay03@googlemail.com>
2019-12-09 03:22:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function viewCart()
   
   for(var i = 0; i < cart.length; i++)
   {
-    cart.length === 1 ? object += ` ${cart[i].itemName} at ${cart[i].itemPrice}.` :
+    cart.length === 1 ? object += ` ${cart[i].itemName} at $${cart[i].itemPrice}.` :
     i === cart.length - 1 ? object += ` and ${itemName} at ${itemPrice}.` :
     object += ` and ${itemName} at ${itemPrice},`
   }
